999a2ea2741394621ff1099f7c04072a9a09e420
Aurelio salgado <aureliosalgado78.@gamil.com>
2023-05-26 14:49:21 -0600

    retornar   Suma y Resta

--- Calculadora/Operaciones.cs
+++ Calculadora/Operaciones.cs
@@ -2,12 +2,12 @@ public class Operaciones
 {
     public int Suma(int a, int b)
     {
-        retum=a+b;
+        returm a+b;
     }
 
     public int Resta(int a, int b)
     {
-        retum=a-b;
+        returm a-b;
     }
 
     public int Multiplicacion(int a, int b)
